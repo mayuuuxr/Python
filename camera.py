7dfebca841ce28bef5f1baef1ce78753ef5a310c
mayuuuxr <118749262+mayuuuxr@users.noreply.github.com>
2024-06-20 15:31:50 +0530

    Add files via upload
    pip install opencv-contrib-python install this package first

---
+++ camera.py
@@ -0,0 +1,15 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+    if cv2.waitKey(33) == ord('a'):
+        cv2.imwrite('image.png',frame)
+
+cap.release()
+cv2.destroyAllWindows()        
